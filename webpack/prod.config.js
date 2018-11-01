473501b8bbc23ee297641db333dffff786bfff1e
matsunagakenta <k_matsunaga@waku-2.com>
2018-11-01 17:15:46 +0900

    fix prod.confg.js

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,6 +1,19 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
 const merge = require('webpack-merge')
 const common = require('./common.config.js')
 
 module.exports = merge(common, {
   mode: 'production',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  }
 })
